6457c1db0bf87831f1206ad6f410f344cbab8657
MsWakesho <wakeshosonja@gmail.com>
2024-03-03 18:36:49 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0, 1]
+    for i in range(2, length):
+        next_fibonacci = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_fibonacci)
+    print(fibonacci_list)
+
+print_fibonacci(9)
